19d715dd599ff591ca81c0f8b44e2a6247c4412d
Vivian <viviancristinavidal@gmail.com>
2024-05-13 09:53:22 -0300

    update index

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express = require("express");
+import * as express from 'express';
+const db = require('./client/db');
 const app = express();
 const port = process.env.PORT || 8080;
 
@@ -16,6 +17,36 @@ app.use(express.urlencoded({ extended: true }));
         password: String 
     }
 */
+
+/*const user = {
+  name: "Joana",
+  email: "j.ana@gmail.com",
+  password: "abc.123#"
+}
+const createdItem = db.create(user)
+console.log(createdItem)
+
+const id = 1
+const item = db.findById(id)
+console.log(item)
+*/
+
+const id = 1
+const user = {
+    name: "Joana Maria",
+    email: "jmaria@gmail.com",
+    password: "123#abc"
+}
+const updatedItem = db.updateById(id, user)
+console.log(updatedItem)
+
+/*const idd = 1
+const removedItem = db.removeById(idd)
+console.log(removedItem)
+
+const items = db.findAll()
+console.log(items)*/
+
 
 app.listen(port, () => {
   console.log(`Esse servidor está rodando em ${port}`);
